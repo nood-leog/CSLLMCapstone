892e5f98a09939765da511d5ca6f9af0e11b15b8
Alex Ryse <aryse54@gmail.com>
2026-03-05 22:42:47 -0800

    db migration, fixed dashboard

---
+++ CSLLMCapstone/Migrations/20260306063738_RemoveGlobalFavoriteAndAddUserFavorites.cs
@@ -0,0 +1,319 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CSLLMCapstone.Migrations
+{
+    /// <inheritdoc />
+    public partial class RemoveGlobalFavoriteAndAddUserFavorites : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsFavorite",
+                table: "Courses");
+
+            migrationBuilder.CreateTable(
+                name: "UserFavorites",
+                columns: table => new
+                {
+                    UserFavoriteId = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    UserId = table.Column<int>(type: "INTEGER", nullable: false),
+                    CourseId = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserFavorites", x => x.UserFavoriteId);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "UserFavorites");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsFavorite",
+                table: "Courses",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.UpdateData(
+                table: "Courses",
+                keyColumn: "CourseId",
+                keyValue: 102,
+                column: "IsFavorite",
+                value: false);
+
+            migrationBuilder.UpdateData(
+                table: "Courses",
+                keyColumn: "CourseId",
+                keyValue: 105,
+                column: "IsFavorite",
+                value: false);
+
+            migrationBuilder.UpdateData(
+                table: "Courses",
+                keyColumn: "CourseId",
+                keyValue: 107,
+                column: "IsFavorite",
+                value: false);
+
+            migrationBuilder.UpdateData(
+                table: "Courses",
+                keyColumn: "CourseId",
+                keyValue: 109,
+                column: "IsFavorite",
+                value: false);
+
+            migrationBuilder.UpdateData(
+                table: "Courses",
+                keyColumn: "CourseId",
+                keyValue: 110,
+                column: "IsFavorite",
+                value: false);
+
+            migrationBuilder.UpdateData(
+                table: "Courses",
+                keyColumn: "CourseId",
+                keyValue: 111,
+                column: "IsFavorite",
+                value: false);
+
+            migrationBuilder.UpdateData(
+                table: "Courses",
+                keyColumn: "CourseId",
+                keyValue: 112,
+                column: "IsFavorite",
+                value: false);
+
+            migrationBuilder.UpdateData(
+                table: "Courses",
+                keyColumn: "CourseId",
+                keyValue: 301,
+                column: "IsFavorite",
+                value: false);
+
+            migrationBuilder.UpdateData(
+                table: "Courses",
+                keyColumn: "CourseId",
+                keyValue: 302,
+                column: "IsFavorite",
+                value: false);
+
+            migrationBuilder.UpdateData(
+                table: "Courses",
+                keyColumn: "CourseId",
+                keyValue: 311,
+                column: "IsFavorite",
+                value: false);
+
+            migrationBuilder.UpdateData(
+                table: "Courses",
+                keyColumn: "CourseId",
+                keyValue: 312,
+                column: "IsFavorite",
+                value: false);
+
+            migrationBuilder.UpdateData(
+                table: "Courses",
+                keyColumn: "CourseId",
+                keyValue: 325,
+                column: "IsFavorite",
+                value: false);
+
+            migrationBuilder.UpdateData(
+                table: "Courses",
+                keyColumn: "CourseId",
+                keyValue: 351,
+                column: "IsFavorite",
+                value: false);
+
+            migrationBuilder.UpdateData(
+                table: "Courses",
+                keyColumn: "CourseId",
+                keyValue: 361,
+                column: "IsFavorite",
+                value: false);
+
+            migrationBuilder.UpdateData(
+                table: "Courses",
+                keyColumn: "CourseId",
+                keyValue: 362,
+                column: "IsFavorite",
+                value: false);
+
+            migrationBuilder.UpdateData(
+                table: "Courses",
+                keyColumn: "CourseId",
+                keyValue: 367,
+                column: "IsFavorite",
+                value: false);
+
+            migrationBuilder.UpdateData(
+                table: "Courses",
+                keyColumn: "CourseId",
+                keyValue: 370,
+                column: "IsFavorite",
+                value: false);
+
+            migrationBuilder.UpdateData(
+                table: "Courses",
+                keyColumn: "CourseId",
+                keyValue: 380,
+                column: "IsFavorite",
+                value: false);
+
+            migrationBuilder.UpdateData(
+                table: "Courses",
+                keyColumn: "CourseId",
+                keyValue: 420,
+                column: "IsFavorite",
+                value: false);
+
+            migrationBuilder.UpdateData(
+                table: "Courses",
+                keyColumn: "CourseId",
+                keyValue: 427,
+                column: "IsFavorite",
+                value: false);
+
+            migrationBuilder.UpdateData(
+                table: "Courses",
+                keyColumn: "CourseId",
+                keyValue: 428,
+                column: "IsFavorite",
+                value: false);
+
+            migrationBuilder.UpdateData(
+                table: "Courses",
+                keyColumn: "CourseId",
+                keyValue: 430,
+                column: "IsFavorite",
+                value: false);
+
+            migrationBuilder.UpdateData(
+                table: "Courses",
+                keyColumn: "CourseId",
+                keyValue: 440,
+                column: "IsFavorite",
+                value: false);
+
+            migrationBuilder.UpdateData(
+                table: "Courses",
+                keyColumn: "CourseId",
+                keyValue: 445,
+                column: "IsFavorite",
+                value: false);
+
+            migrationBuilder.UpdateData(
+                table: "Courses",
+                keyColumn: "CourseId",
+                keyValue: 446,
+                column: "IsFavorite",
+                value: false);
+
+            migrationBuilder.UpdateData(
+                table: "Courses",
+                keyColumn: "CourseId",
+                keyValue: 450,
+                column: "IsFavorite",
+                value: false);
+
+            migrationBuilder.UpdateData(
+                table: "Courses",
+                keyColumn: "CourseId",
+                keyValue: 452,
+                column: "IsFavorite",
+                value: false);
+
+            migrationBuilder.UpdateData(
+                table: "Courses",
+                keyColumn: "CourseId",
+                keyValue: 456,
+                column: "IsFavorite",
+                value: false);
+
+            migrationBuilder.UpdateData(
+                table: "Courses",
+                keyColumn: "CourseId",
+                keyValue: 457,
+                column: "IsFavorite",
+                value: false);
+
+            migrationBuilder.UpdateData(
+                table: "Courses",
+                keyColumn: "CourseId",
+                keyValue: 460,
+                column: "IsFavorite",
+                value: false);
+
+            migrationBuilder.UpdateData(
+                table: "Courses",
+                keyColumn: "CourseId",
+                keyValue: 465,
+                column: "IsFavorite",
+                value: false);
+
+            migrationBuilder.UpdateData(
+                table: "Courses",
+                keyColumn: "CourseId",
+                keyValue: 466,
+                column: "IsFavorite",
+                value: false);
+
+            migrationBuilder.UpdateData(
+                table: "Courses",
+                keyColumn: "CourseId",
+                keyValue: 467,
+                column: "IsFavorite",
+                value: false);
+
+            migrationBuilder.UpdateData(
+                table: "Courses",
+                keyColumn: "CourseId",
+                keyValue: 470,
+                column: "IsFavorite",
+                value: false);
+
+            migrationBuilder.UpdateData(
+                table: "Courses",
+                keyColumn: "CourseId",
+                keyValue: 471,
+                column: "IsFavorite",
+                value: false);
+
+            migrationBuilder.UpdateData(
+                table: "Courses",
+                keyColumn: "CourseId",
+                keyValue: 473,
+                column: "IsFavorite",
+                value: false);
+
+            migrationBuilder.UpdateData(
+                table: "Courses",
+                keyColumn: "CourseId",
+                keyValue: 480,
+                column: "IsFavorite",
+                value: false);
+
+            migrationBuilder.UpdateData(
+                table: "Courses",
+                keyColumn: "CourseId",
+                keyValue: 481,
+                column: "IsFavorite",
+                value: false);
+
+            migrationBuilder.UpdateData(
+                table: "Courses",
+                keyColumn: "CourseId",
+                keyValue: 489,
+                column: "IsFavorite",
+                value: false);
+        }
+    }
+}
